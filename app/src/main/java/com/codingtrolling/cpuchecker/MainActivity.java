6239e0a0c669b4d0b1e0ab4709e4e1cfd60de9fa
codingtrolling <rabi65171@gmail.com>
2026-03-17 07:37:38 +0000

    Feat: Integrated Battery Diagnostic and Power Monitoring

--- app/src/main/java/com/codingtrolling/cpuchecker/MainActivity.java
+++ app/src/main/java/com/codingtrolling/cpuchecker/MainActivity.java
@@ -1,5 +1,9 @@
 package com.codingtrolling.cpuchecker;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -26,29 +30,32 @@ public class MainActivity extends AppCompatActivity {
                 StringBuilder sb = new StringBuilder();
                 int cores = Runtime.getRuntime().availableProcessors();
                 
-                // --- SoC Header (Aligned with %-18s) ---
+                // --- SoC Section ---
                 sb.append(String.format("%-18s %s\n", "Model", "MT6769H"));
                 sb.append(String.format("%-18s %s\n", "Cores", cores));
-                sb.append(String.format("%-18s %s\n", "big.LITTLE", "Yes (2 clusters)"));
                 sb.append(String.format("%-18s %s\n", "Process", "12 nm"));
                 sb.append("------------------------------------\n");
 
-                // --- Live Core Speeds ---
+                // --- CPU Core Speeds ---
                 for (int i = 0; i < cores; i++) {
-                    String freq = getCoreFreq(i);
-                    sb.append(String.format("%-18s %s MHz\n", "CPU " + i, freq));
+                    sb.append(String.format("%-18s %s MHz\n", "CPU " + i, getCoreFreq(i)));
                 }
 
                 sb.append("------------------------------------\n");
+
+                // --- Battery Section ---
+                Intent batteryIntent = registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+                int level = batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+                int temp = batteryIntent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0);
+                int voltage = batteryIntent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, 0);
                 
-                // --- GPU & System ---
-                sb.append(String.format("%-18s %s\n", "GPU Vendor", "ARM"));
+                sb.append(String.format("%-18s %d%%\n", "Battery Level", level));
+                sb.append(String.format("%-18s %.1f°C\n", "Temperature", (temp / 10.0)));
+                sb.append(String.format("%-18s %d mV\n", "Voltage", voltage));
                 sb.append(String.format("%-18s %s\n", "GPU Renderer", "Mali-G52 MC2"));
-                sb.append(String.format("%-18s %s\n", "Scaling Gov", "schedutil"));
 
                 final String result = sb.toString();
                 handler.post(() -> output.setText(result));
-                
                 try { Thread.sleep(1000); } catch (Exception e) {}
             }
         }).start();
